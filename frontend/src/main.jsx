5d6d74c6d10fc798ecbc735ebf7c2a87f66802e6
Eduardo Coutinho da Silva Junior <eduardo.coutinho@sga.pucminas.br>
2025-05-10 11:39:45 -0300

    Corrigido o script dev no package.json e ajustes na HomePage.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,12 @@
 import HomePage from './Pages/HomePage.jsx';
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './styles/globals.css';
 
-import Login from './Pages/Login.jsx'
-import Cadastro from './Pages/Cadastro.jsx'
-import ResetSenha from './Pages/ResetSenha.jsx'
+import Login from './Pages/Login.jsx';
+import Cadastro from './Pages/Cadastro.jsx';
+import ResetSenha from './Pages/ResetSenha.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -15,8 +15,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/reset-senha" element={<ResetSenha />} />
-        
+        <Route path="/home" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
-)
+);
