e665f00da7c862c618492f52871ee13c178aa6c2
artma1 <artma30@gmail.com>
2025-05-13 21:45:40 -0300

    aa

--- Backend/Nextflix/Models/User.cs
+++ Backend/Nextflix/Models/User.cs
@@ -20,17 +20,3 @@ namespace Nextflix.Models
     //public List<Comment> Comments { get; set; } = new();
   }
 }
-
-public class CustomFilmList
-{
-  public int Id { get; set; }
-
-  public string Name { get; set; } 
-
-  public int UserId { get; set; }
-
-  [JsonIgnore]
-  public User User { get; set; }
-
-  public List<Film> Films { get; set; } = new();
-}
