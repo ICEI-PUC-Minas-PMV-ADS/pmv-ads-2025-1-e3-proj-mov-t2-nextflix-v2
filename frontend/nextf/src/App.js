8a1de67eceef8c6b9b41a85ff8486408a44e0af4
artma1 <artma30@gmail.com>
2025-06-01 19:31:54 -0300

    Perfil de usuário e edição/cadastro de usuário ok

--- frontend/nextf/src/App.js
+++ frontend/nextf/src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import User from './screens/User';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import UserProfile from './screens/UserProfile';
+import UserCreate from './screens/UserCreate';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
-  return <User />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="UserProfile">
+        <Stack.Screen name="UserProfile" component={UserProfile} options={{ title: 'Detalhes do Usuário' }} />
+        <Stack.Screen name="UserCreate" component={UserCreate} options={{ title: 'Criar Usuário' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
